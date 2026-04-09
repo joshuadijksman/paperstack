b57b6ec107391ae672702e00904462ed6d2f8241
Mayohills <manou.lskr@gmail.com>
2026-04-09 13:11:46 +0200

    Improved data processing
    I split up the jupyter notebook and my own written functions into two files. Everything works for now. The data processing has improved by removing excess lines. If it doesnt work with a certain dataset it is now easier to check where it went wrong because i improved the visuals significantly.

--- Manou_version/code/selfmadefunctions.py
+++ Manou_version/code/selfmadefunctions.py
@@ -1,8 +1,11 @@
-import math
-import numpy as np
-from pathlib import Path
 import pandas as pd
-import csv
+import numpy as np
+import matplotlib.pyplot as plt
+from lmfit import Model
+from scipy.stats import chi2
+import math as math
+from scipy.ndimage import gaussian_filter1d
+from pathlib import Path
 
 # The camera from the measurements of Maria and Manou was turned 90 degrees, 
 # making positive movement in X equal negative vertical movement. 
@@ -33,6 +36,103 @@ def coordinate_swap(filename, folder):    # folder = metingen_0_40 of metingen_0
 
     cleaned_file.to_csv(f"{filename}_clean.csv", index = False)
 
+def databewerken(networkfolder, filename, thickness):
+  
+    # tweak these
+    N_points = 60
+    leniency = 5
+    y_err = 1
 
-coordinate_swap("V_d0_1", "metingen_0_40")
+    # maybe tweak these for better results
+    sigma = 2
+    
+    # dont tweak these
+    Total_first_N_points = 0
+    delete_first_elements = 0
+    laagtepunt_1 = 0
+    laagtepunt_2 = 0
+    
+
+    file_path = networkfolder / f"{filename}.csv"
+    data_current =  pd.read_csv(file_path)
+
+    y_points = data_current.iloc[:, 1] #Camera hangt scheef, dus omdraaien en lezen van de x-coordinaten.
+    frames = data_current.iloc[:, 0]
+
+    # ervoor zorgen dat eerste rechte data wordt afgeknipt
+    for i in range(N_points):           # gemiddelde nemen van eerste N punten
+        Total_first_N_points += y_points[i]
+    average_first_N_points = Total_first_N_points / N_points
+
+
+    while abs(y_points[delete_first_elements] - average_first_N_points) < leniency:        # wachten tot een punt te ver van het gemiddelde van de eerste N af zit. AKA wanneer valt het?
+        delete_first_elements += 1
+
+    afgeknipt_y = y_points[delete_first_elements:]     # Deze punten afknippen
+    afgeknipt_frame = frames[delete_first_elements:]   # Deze punten afknippen
+
+    smoothed = gaussian_filter1d(afgeknipt_y, sigma = sigma)     # hier een gaussisch filter overheen halen, zodat alle punten mooi zijn
+
+    for i in range(len(smoothed) - 1):
+        if smoothed[i] < smoothed[i + 1] and smoothed[i] < smoothed[i-1]:      # van deze data de eerste twee minimums vinden en het frame hiervan onthouden
+            print(f"Minimum at {i} frames.")
+            if laagtepunt_1 == 0:
+                laagtepunt_1 = i + 2
+            else:
+                laagtepunt_2 = i - 2
+                break
+    
+    y = [0, average_first_N_points]
+    x1 = [laagtepunt_1 + delete_first_elements, laagtepunt_1 + delete_first_elements]
+    x2 = [laagtepunt_2 + delete_first_elements, laagtepunt_2 + delete_first_elements]
+
+    frame_bounce = afgeknipt_frame[laagtepunt_1:laagtepunt_2]
+    y_bounce = afgeknipt_y[laagtepunt_1:laagtepunt_2]
+
+    fig, ax = plt.subplots(1, 3, figsize=(18, 5))
+    print(f'Thickness = {thickness}')
+    print(f"The first {delete_first_elements} frames are deleted, after that the ball drops.")
+    print(f'The ball is released at y = {average_first_N_points} pixels.')
+
+    # Grafiek 1
+    ax[0].errorbar(frames, y_points, yerr=y_err)
+    ax[0].plot(
+        [delete_first_elements - 200, delete_first_elements + 200],
+        [average_first_N_points, average_first_N_points],
+        'b--'
+    )
+    ax[0].plot(
+        [delete_first_elements, delete_first_elements],
+        [average_first_N_points + 100, 0],
+        'r--'
+    )
+    ax[0].set_xlabel('Time [frames]')
+    ax[0].set_ylabel('Height [pixels]')
+    ax[0].set_title('Beginning Data')
+
+    # Grafiek 2
+    ax[1].errorbar(afgeknipt_frame, afgeknipt_y, yerr=y_err)
+    ax[1].plot(x1, y, 'r--')
+    ax[1].plot(x2, y, 'r--')
+    ax[1].set_xlabel('Time [frames]')
+    ax[1].set_ylabel('Height [pixels]')
+    ax[1].set_title('Data from moment of release')
+
+    # Grafiek 3
+    ax[2].errorbar(
+        frame_bounce,
+        y_bounce,
+        yerr=y_err,
+        markersize=2,
+        fmt='o'
+    )
+    ax[2].set_xlabel('Time [frames]')
+    ax[2].set_ylabel('Height [pixels]')
+    ax[2].set_title('Isolated first Bounce')
+
+    fig.suptitle(f'Measurement on thickness {thickness}, filename = {filename}')
+    plt.tight_layout()
+    plt.show()
+
+    return average_first_N_points, frame_bounce, y_bounce  #Returns the drop height en the trajectory of the relevant bounce.
 
